4e8264636f36b23227d22bc9e059f180fba6a179
Andrew Laptev <a.d.laptev@gmail.com>
2015-03-02 17:22:06 +0300

    Update lab0.hs

--- lab0/lab0.hs
+++ lab0/lab0.hs
@@ -8,10 +8,13 @@ import qualified Data.ByteString.Char8 as C
 
 (email, name) = ("", encodeUtf8 "") -- адрес почты и фамилия с инициалами
 
+pascal :: Int -> Int -> Int
 pascal c r = 1 -- а тут решение
 
+printIt :: Int -> C.ByteString
 printIt n = C.pack $ show $ [0..n] >>= (\x -> map (\y -> pascal y x) [0..x])
 
+main :: IO()
 main = do
   initReq <- parseUrl "http://mipt.eu01.aws.af.cm/lab0"
   let req = urlEncodedBody [("email", email), ("name", name), ("lang", "haskell"), ("content", printIt 20)] $ initReq { method = "POST" }
