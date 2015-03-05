6ec3ee399ecba60abf0395a8f56c8eea09e5c6e5
Andrew Laptev <a.d.laptev@gmail.com>
2015-03-05 20:00:32 +0300

    Update lab0.fs

--- lab0/lab0.fs
+++ lab0/lab0.fs
@@ -10,7 +10,7 @@ let pascal c r = 1 // а тут решение
 
 let printIt n = 
   "[" +
-  ([for x in 0..n do yield! List.map (fun y -> pascal y x) [0..x]] 
+  ([for x in 0..n do for y in 0..x do yield pascal y x] 
     |> List.map (fun x -> x.ToString())
     |> List.reduce (fun x y -> x + "," + y) )
   + "]"
