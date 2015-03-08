9fbb4de207f81ae0aba53377d7fe2e17cfd4c1d2
Andrew Laptev <a.d.laptev@gmail.com>
2015-03-08 09:00:04 +0300

    Windows workaround

--- lab0/lab0.hs
+++ lab0/lab0.hs
@@ -5,6 +5,7 @@ import Network.HTTP.Conduit
 import Data.Text.Encoding
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Char8 as C
+import Network (withSocketsDo)
 
 (email, name) = ("", encodeUtf8 "") -- адрес почты и фамилия с инициалами
 
@@ -15,7 +16,8 @@ printIt :: Int -> C.ByteString
 printIt n = C.pack $ show $ [pascal y x | x <- [0..n], y <- [0..x]]
 
 main :: IO()
-main = do
+main = 
+  withSocketsDo $ do
   initReq <- parseUrl "http://mipt.eu01.aws.af.cm/lab0"
   let req = urlEncodedBody [("email", email), ("name", name), ("lang", "haskell"), ("content", printIt 20)] $ initReq { method = "POST" }
   response <- withManager $ httpLbs req
