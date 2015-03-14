ddbffdf9ffd857cb3888354e49cad41ab9e6fc94
Andrew Laptev <a.d.laptev@gmail.com>
2015-03-14 14:22:05 +0300

    Update lab1.fs

--- lab1/lab1.fs
+++ lab1/lab1.fs
@@ -43,7 +43,7 @@ let printSolve () =
 let main () = 
   let values = new NameValueCollection()
   values.Add("email", email)
-  values.Add("content", File.ReadAllText(__SOURCE_DIRECTORY__ + @"\" + __SOURCE_FILE__))
+  values.Add("content", File.ReadAllText(__SOURCE_DIRECTORY__ + @"/" + __SOURCE_FILE__))
 
   let client = new WebClient()
   let response = client.UploadValues(new Uri("http://mipt.eu01.aws.af.cm/lab1"), values)
